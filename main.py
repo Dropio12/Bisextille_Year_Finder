e9343b1fb2ff16be708969b963e7bf5c5da73e9a
anass <anass.elhallaoui@gmail.com>
2022-10-11 10:30:42 -0400

    Little upgrade to show only bisextille years

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import Bisextille_Finder as bf
 def main():
     annee_début = int(input("Entrez une année de début: "))
     annee_fin = int(input("Entrez une année de fin: "))
+    print("Les années bisextilles entre "+ str(annee_début)+" et "+str(annee_fin)+" sont:")
     for i in range(annee_début, annee_fin+1):
         if bf.Determiner_Annee_Bisextille(i)== True:
-            print(str(i) + " est bisextille")
-        else:
-            print(str(i)+" n'est pas bisextille")
-
+            print(str(i))
 if __name__ == "__main__":
     main()
